3f5f4521c4153acf17fb2a778dd4383dcc348c42
danishsshaikh <danishsshaikh@gmail.com>
2019-12-17 03:07:44 +0530

    :ehGuess: Update

--- Assets/Scripts/NumberCON.cs
+++ Assets/Scripts/NumberCON.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NumberCON : MonoBehaviour
 {
     [SerializeField] int min;
     [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI ehGuess;
     int guess;
     void Start()
     {
@@ -16,6 +18,7 @@ public class NumberCON : MonoBehaviour
     {
         max = max + 1;
         guess = (max + min) / 2;
+        ehGuess.text = guess.ToString();
     }
 
     void NextGuess()
