c92bbf7e54803d2d8b6f02a514e321508646f2b2
danishsshaikh <danishsshaikh@gmail.com>
2019-12-17 02:52:14 +0530

    cleared debugs

--- Assets/Scripts/NumberCON.cs
+++ Assets/Scripts/NumberCON.cs
@@ -17,17 +17,6 @@ public class NumberCON : MonoBehaviour
         max = 1000;
         min = 1;
         guess = 500;
-
-        Debug.Log("Welcome, to our Number Game");
-        Debug.Log("Hosted by Danish Shaikh, @danishsshaikh");
-        Debug.Log("Now just think of an number between 1 to 1000");
-
-        Debug.Log("May the Lowest Number to Pick be:" + min);
-        Debug.Log("May the Highest Number to Pick be:" + max);
-
-        Debug.Log("Tell me if your number is higher or lower than:" + guess);
-        Debug.Log("Arrow Up = Higher, Arrow Down = Lower, Enter = Correct");
-
         max = max + 1;
     }
 
