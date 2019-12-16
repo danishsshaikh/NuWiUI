4547cd6e43c756807f91068a8a6b376f26ed936b
danishsshaikh <danishsshaikh@gmail.com>
2019-12-17 02:31:42 +0530

    Update LastScreenLoader.cs

--- Assets/Scripts/LastScreenLoader.cs
+++ Assets/Scripts/LastScreenLoader.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class LastScreenLoader : MonoBehaviour
 {
+    public GameObject MainUIScreen;
     public void LastScreen()
     {
-        //SceneManager.LoadScene(MainUIScreen);
-        int currentSceneIndex = 2 ;
-        SceneManager.LoadScene(currentSceneIndex - 2);
+        SceneManager.LoadScene(MainUIScreen);
+        //int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        //SceneManager.LoadScene(currentSceneIndex - 1);
     }
 }
